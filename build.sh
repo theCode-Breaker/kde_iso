0939ce5f83d2747e9a541097adccd93fd7139e42
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-03-14 23:42:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-pacman -S --noconfirm --needed archlinux-keyring
+pacman -S --noconfirm --needed archlinux-keyring wget
+wget https://raw.githubusercontent.com/theCode-Breaker/kde_iso/main/archiso/airootfs/etc/pacman.d/xero-mirrorlist -O /etc/pacman.d/xero-mirrorlist
 mkarchiso -v -w ./work -o ./out ./archiso/
 echo "ISO Built in ./out folder!"
